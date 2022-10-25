408f5f9927f52c15b972036486f53f63211e6ed3
DigitalGro0ve <vtorc17@freeuni.edu.ge>
2022-10-25 17:14:09 +0400

    js task 2

---
+++ script.js
@@ -0,0 +1,20 @@
+// var numbers = [1,2,55,17]
+// for(var i=0; i<numbers.length; i++){
+//   document.write(numbers[i]);
+// }
+  
+
+
+// function money(n){
+//     if(n<=20){
+//         return("იაფია");
+//     }
+//     else if(n>20 && n<=50){
+//         return("საშუალოა");
+//         }
+//     else{ 
+//         return("ძვირია"); 
+//     }
+// }
+// var result = money(500);
+// document.write(result)
